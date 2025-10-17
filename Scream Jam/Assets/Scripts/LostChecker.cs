7365f7d3744d484ee101e84263fc71c0b68d7e0a
SecretSilvio <nmc322@drexel.edu>
2025-10-16 21:43:01 -0400

    lostchecker

---
+++ Scream Jam/Assets/Scripts/LostChecker.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+using static UnityEngine.GraphicsBuffer;
+
+public class LostChecker : MonoBehaviour
+{
+    public GameObject mom;
+    public GameObject player;
+
+    private float currentDistance;
+    public float LostThreshold = 20f;
+    public float TimeToLose = 5f;
+
+    public Volume volume;
+    private Vignette vignette;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        mom = FindFirstObjectByType<MomController>().gameObject;
+        player = FindFirstObjectByType<PlayerMovement>().gameObject;
+
+        if (volume.profile.TryGet<Vignette>(out vignette))
+        {
+            vignette.intensity.value = 0f;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentDistance = Vector2.Distance(new Vector2(mom.transform.position.x, mom.transform.position.z), new Vector2(player.transform.position.x, player.transform.position.z));
+        if (currentDistance > LostThreshold)
+        {
+            StartCoroutine(LostCoroutine());
+        }
+    }
+
+    public IEnumerator LostCoroutine()
+    {
+        float timerElapsed = 0f;
+        while (timerElapsed < TimeToLose)
+        {
+            if (currentDistance <= LostThreshold)
+            {
+                if (vignette != null)
+                {
+                    vignette.intensity.value = 0f;
+                }
+                yield break; // Exit the coroutine if the player is back within the threshold
+            }
+            float t = timerElapsed / TimeToLose;
+            vignette.intensity.value = Mathf.Lerp(0, 1, t);
+
+            timerElapsed += Time.deltaTime;
+            yield return null;
+        }
+        vignette.intensity.value = 1; // Ensure it ends exactly at target
+    }
+}
